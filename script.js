52ca17ce423bc8a2cc307720e10c88911279da04
henriquevital00 <henriquevital1000@hotmail.com>
2022-08-13 00:20:54 -0300

    Uploading song

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function move(el){
     var changeTop = Math.floor((Math.random() * (wdh - height + 1)));
     var changeLeft = Math.floor((Math.random() * (wdw - width + 1))) ;
     console.log("Height: " + changeTop + " Width: " + changeLeft);
-    
+
     el.style.marginTop = changeTop + "px";
     el.style.marginLeft = changeLeft + "px";
 }
@@ -19,4 +19,10 @@ function setHeart(element){
     document.getElementById("botao").style.display = "none";
     document.getElementById("botao2").style.display = "none";
     document.getElementById("heart").style.display = "block";
+    playSong();
+}
+
+function playSong(){
+    let snd = new Audio("love.mp3");
+    snd.play();
 }
